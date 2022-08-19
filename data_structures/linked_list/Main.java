4b75c345111a1817587ca6b6dc834751a87519b3
Samuel Njenga <snjenga704@gmail.com>
2022-08-19 12:26:42 +0300

    feat: add LinkedList.java class which contains insert, insertAtStart and show method algorithms of a LinkedList

---
+++ data_structures/linked_list/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        LinkedList list = new LinkedList();
+        list.insert(5);
+        list.insert(45);
+        list.insert(12);
+        list.insertAtStart(26);
+        list.show();
+    }
+}
